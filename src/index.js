858ac16dad09faa7f85674a8c1904530b4601236
Rezarafi97 <rezarafi901@gmail.com>
2023-04-01 14:59:44 +0430

    fixing bugs 2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="contacts-manager-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
